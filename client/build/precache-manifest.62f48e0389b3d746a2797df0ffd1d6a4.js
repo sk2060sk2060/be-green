ee154f93b1f744745bf5c5b2b5d69e3bb8f48614
sk <sk2060sk2060@gmail.com>
2020-05-09 13:29:46 -0600

    add build folder

---
+++ client/build/precache-manifest.62f48e0389b3d746a2797df0ffd1d6a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d64cf1590b72f0a0175",
+    "url": "/static/css/main.23ebc00f.chunk.css"
+  },
+  {
+    "revision": "1d64cf1590b72f0a0175",
+    "url": "/static/js/main.e5601afa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb17bcf018bf7c55997e",
+    "url": "/static/js/2.09d13a7d.chunk.js"
+  },
+  {
+    "revision": "0c9f2c07b9fae5930d42e2272e076d1b",
+    "url": "/index.html"
+  }
+];
