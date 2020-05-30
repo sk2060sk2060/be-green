9b00fec7f0741389799e07cbb88b28f780d5ad20
sk <sk2060sk2060@gmail.com>
2020-05-30 15:14:16 -0600

    trying to fix heroku issue

---
+++ client/build/precache-manifest.48b7cd3ed5ce29d37df69b68e7db086b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b9a43c95db8dc02fda6",
+    "url": "/static/css/main.0cc1671e.chunk.css"
+  },
+  {
+    "revision": "3b9a43c95db8dc02fda6",
+    "url": "/static/js/main.15d3d322.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffa0825ae23ff5338010",
+    "url": "/static/js/2.aaab4530.chunk.js"
+  },
+  {
+    "revision": "cf582897ba8ce23a867d16b9909d7c77",
+    "url": "/static/media/recyclelogo.cf582897.jpg"
+  },
+  {
+    "revision": "27ff0c250ee639470c3c064d73147ff2",
+    "url": "/static/media/owlartcraft.27ff0c25.jpg"
+  },
+  {
+    "revision": "4eaa59443cd3f5a2b028cb737f77a7df",
+    "url": "/static/media/bottleholder.4eaa5944.jpg"
+  },
+  {
+    "revision": "51a56fa0d812f2b50f37677b590daa69",
+    "url": "/static/media/bird-feeder-1.51a56fa0.jpg"
+  },
+  {
+    "revision": "2d3d17e0dc842c61fab6dd08fc551d87",
+    "url": "/static/media/minion-flower-planter-pot.2d3d17e0.jpg"
+  },
+  {
+    "revision": "3e5a0aec8214f16e8c56e19f43c7f39a",
+    "url": "/static/media/tire-project.3e5a0aec.jpg"
+  },
+  {
+    "revision": "71967edb59610e28003d4f6e503d09c5",
+    "url": "/static/media/light-bulb-art.71967edb.png"
+  },
+  {
+    "revision": "73bc99da60e52c800a71a0e617d70ef9",
+    "url": "/index.html"
+  }
+];
