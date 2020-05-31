ef8976f2e77af5f5f786fca9ee40edfba1912182
sk <sk2060sk2060@gmail.com>
2020-05-30 21:57:19 -0600

    trying to fix heroku issue

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,62 +1,3 @@
-// import axios from "axios";
-
-// export default {
-//   // Gets all books
-//   // getMaps: function () {
-//   //   return axios.get(
-//   //     `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=48.8583701&longitude=2.2922926`
-//   //   );
-//   // },
-//   getMaps: function (latitude=48.8583701,longitude=2.2922926) {
-//     console.log(latitude, longitude);
-//     return axios.get(
-//       `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}`
-//     );
-//   },  
-//   getMaterial: async function (topic) {
-//     return await axios.get(
-//       `https://api.earth911.com/earth911.searchMaterials?api_key=7827591ea2eb59e9&query=${topic}`
-//     );
-//   },
-//   // getID: function (materialIds) {
-//   //   return axios.get(
-//   //     `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=39.742043&longitude=-104.991531&${materialIds
-//   //       .map((materialId) => "material_id[]=" + materialId + "&")
-//   //       .join("")}`
-//   //   );
-//   // },
-//   // 
-
-//   getID: function (materialIds, latitude=39.742043, longitude=-104.991531) {
-//     const materialIdMap = materialIds.map((materialId) =>  materialId )
-//     console.log(materialIdMap);
-    
-//     return axios.get(
-//       `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}&${materialIdMap}`
-//     );
-//   },  
-//   getSave: function (point) {
-//     console.log(point);
-//     axios({
-//       method: "post",
-//       url: "/api/users/fav",
-//       data: {
-//         favorite: point,
-//       },
-//     });
-//   },
-//   getAddress: async function(latitude, longitude) {
-//     return await axios.get(
-//       `https://us1.locationiq.com/v1/reverse.php?key=ec3dca5fbce16a&lat=${latitude}&lon=${longitude}&format=json`
-//     );
-//   }
-// };
-// //Users.findOneAndUpdate({_id: req.user.id}, {$push: {favorites: req.body}});
-// //};
-
-
-
-
 import axios from "axios";
 
 export default {
@@ -86,26 +27,14 @@ export default {
   // },
   // 
 
-  // getID: async function (materialIds, latitude=39.742043, longitude=-104.991531) {
-  //   const materialIdMap = materialIds.map((materialId) =>  materialId )
-  //   console.log(materialIdMap);
-    
-  //   return await axios.get(
-  //     `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}&${materialIdMap}`
-  //   );
-  // },  
-
-  getID: async function (materialIds, latitude=39.742043, longitude=-104.991531) {
+  getID: function (materialIds, latitude=39.742043, longitude=-104.991531) {
     const materialIdMap = materialIds.map((materialId) =>  materialId )
     console.log(materialIdMap);
     
-    return await axios.get(
-      `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=39.742043&longitude=-104.991531&${materialIds
-        .map((materialId) => "material_id[]=" + materialId + "&")
-        .join("")}`
+    return axios.get(
+      `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}&${materialIdMap}`
     );
-  },   
-
+  },  
   getSave: function (point) {
     console.log(point);
     axios({
@@ -124,3 +53,7 @@ export default {
 };
 //Users.findOneAndUpdate({_id: req.user.id}, {$push: {favorites: req.body}});
 //};
+
+
+
+
