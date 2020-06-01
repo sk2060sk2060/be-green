10d0df6e0eb43f2101d0cff03110ca88987324c1
Carina Manent <carinamoon17@gmail.com>
2020-06-01 11:46:10 -0600

    heroku issues

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,12 +7,12 @@ export default {
   //     `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=48.8583701&longitude=2.2922926`
   //   );
   // },
-  getMaps: function (latitude=48.8583701,longitude=2.2922926) {
+  getMaps: function (latitude = 48.8583701, longitude = 2.2922926) {
     console.log(latitude, longitude);
     return axios.get(
       `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}`
     );
-  },  
+  },
   getMaterial: async function (topic) {
     return await axios.get(
       `https://api.earth911.com/earth911.searchMaterials?api_key=7827591ea2eb59e9&query=${topic}`
@@ -25,16 +25,16 @@ export default {
   //       .join("")}`
   //   );
   // },
-  // 
+  //
 
-  getID: function (materialIds, latitude=39.742043, longitude=-104.991531) {
-    const materialIdMap = materialIds.map((materialId) =>  materialId )
-    console.log(materialIdMap);
-    
+  getID: function (materialIds, latitude = 39.742043, longitude = -104.991531) {
+    const materialIdMap = materialIds.map((materialId) => materialId);
+    console.log("--->", materialIds, materialIdMap);
+
     return axios.get(
       `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}&${materialIdMap}`
     );
-  },  
+  },
   getSave: function (point) {
     console.log(point);
     axios({
@@ -45,11 +45,11 @@ export default {
       },
     });
   },
-  getAddress: async function(latitude, longitude) {
+  getAddress: async function (latitude, longitude) {
     return await axios.get(
       `https://us1.locationiq.com/v1/reverse.php?key=ec3dca5fbce16a&lat=${latitude}&lon=${longitude}&format=json`
     );
-  }
+  },
 };
 //Users.findOneAndUpdate({_id: req.user.id}, {$push: {favorites: req.body}});
 //};
