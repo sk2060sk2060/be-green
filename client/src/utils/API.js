bedb45a6b6306539fa89a05fe8528f5971e0d4d2
Carina Manent <carinamoon17@gmail.com>
2020-06-01 11:58:02 -0600

    fixed heroku issues

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,12 +7,12 @@ export default {
   //     `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=48.8583701&longitude=2.2922926`
   //   );
   // },
-  getMaps: function (latitude=48.8583701,longitude=2.2922926) {
+  getMaps: function (latitude = 48.8583701, longitude = 2.2922926) {
     console.log(latitude, longitude);
     return axios.get(
-      `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}`
+      `https://api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}`
     );
-  },  
+  },
   getMaterial: function (topic) {
     return axios.get(
       `https://api.earth911.com/earth911.searchMaterials?api_key=7827591ea2eb59e9&query=${topic}`
@@ -25,13 +25,13 @@ export default {
   //       .join("")}`
   //   );
   // },
-  getID: function (materialIds, latitude=39.742043, longitude=-104.991531) {
+  getID: function (materialIds, latitude = 39.742043, longitude = -104.991531) {
     return axios.get(
-      `https:api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}&${materialIds
+      `https://api.earth911.com/earth911.searchLocations?api_key=7827591ea2eb59e9&latitude=${latitude}&longitude=${longitude}&${materialIds
         .map((materialId) => "material_id[]=" + materialId + "&")
         .join("")}`
     );
-  },  
+  },
   getSave: function (point) {
     console.log(point);
     axios({
@@ -42,11 +42,11 @@ export default {
       },
     });
   },
-  getAddress: async function(latitude, longitude) {
+  getAddress: async function (latitude, longitude) {
     return await axios.get(
       `https://us1.locationiq.com/v1/reverse.php?key=ec3dca5fbce16a&lat=${latitude}&lon=${longitude}&format=json`
     );
-  }
+  },
 };
 //Users.findOneAndUpdate({_id: req.user.id}, {$push: {favorites: req.body}});
 //};
